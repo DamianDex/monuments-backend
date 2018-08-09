7b6c62cd0797b152a882fc51366dcbb3e779aa95
farad <damian.fara@pega.com>
2018-08-09 09:14:59 +0200

    Initial commit

---
+++ src/main/java/monumentsbackend/monumentsbackend/MonumentsBackendApplication.java
@@ -0,0 +1,12 @@
+package monumentsbackend.monumentsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonumentsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonumentsBackendApplication.class, args);
+	}
+}
